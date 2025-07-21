4b7e96d19e2d3d6f51355cc152a85fdec6b27cb2
Rui <itourui.i@icloud.com>
2025-07-21 10:23:23 +0900

    feat(logger): introduce logger package enhancements (#21)
    Added the foundational files for the logger package, including configuration, documentation, and integration with web applications.
    
    ---
    ItouRui --> feat(logger): add configuration and setup files for logger package
    ItouRui --> feat(logger): add base and types files for logging
    ItouRui --> feat(logger): add NextStructuredLogger class
    ItouRui --> feat(logger): add README for logger package
    ItouRui --> chore(github): add logger label
    ItouRui --> feat(web): add logger dependency
    ---

---
+++ packages/logger/src/next.ts
@@ -0,0 +1,97 @@
+import { BaseLogger } from './base';
+import type { LogContext, LoggerOptions } from './types';
+
+/**
+ * A structured logger for NextJS applications.
+ */
+export class NextStructuredLogger extends BaseLogger {
+  /**
+   * Creates an instance of NextStructuredLogger.
+   * @param options - The options for the logger.
+   */
+  constructor(options: LoggerOptions = {}) {
+    super({
+      name: options.name || 'NextJS',
+      logLevel: options.level || 'info',
+      format: options.format || 'text',
+    });
+  }
+
+  /**
+   * Logs a message with 'info' severity.
+   * @param message - The message to log.
+   * @param context - The context of the log message.
+   */
+  log(message: string, context?: LogContext): void {
+    if (!this.isLevelEnabled('log')) return;
+    const { message: msg, params } = this.extractMessages([message, context]);
+    this.printMessage({ message: msg, params, context: null, severity: 'info' });
+  }
+
+  /**
+   * Logs a debug message.
+   * @param message - The debug message.
+   * @param context - The context of the log message.
+   */
+  debug(message: string, context?: LogContext): void {
+    if (!this.isLevelEnabled('debug')) return;
+    const { message: msg, params } = this.extractMessages([message, context]);
+    this.printMessage({ message: msg, params, context: null, severity: 'debug' });
+  }
+
+  /**
+   * Logs an info message.
+   * @param message - The info message.
+   * @param context - The context of the log message.
+   */
+  info(message: string, context?: LogContext): void {
+    if (!this.isLevelEnabled('info')) return;
+    const { message: msg, params } = this.extractMessages([message, context]);
+    this.printMessage({ message: msg, params, context: null, severity: 'info' });
+  }
+
+  /**
+   * Logs a warning message.
+   * @param message - The warning message.
+   * @param context - The context of the log message.
+   */
+  warn(message: string, context?: LogContext): void {
+    if (!this.isLevelEnabled('warn')) return;
+    const { message: msg, params } = this.extractMessages([message, context]);
+    this.printMessage({ message: msg, params, context: null, severity: 'warn' });
+  }
+
+  /**
+   * Logs an error message.
+   * @param message - The error message or Error object.
+   * @param context - The context of the log message.
+   */
+  error(message: string | Error, context?: LogContext): void {
+    if (!this.isLevelEnabled('error')) return;
+
+    if (message instanceof Error) {
+      const { message: msg, stack, params } = this.extractMessagesWithStack([message, context]);
+      this.printMessage({ message: msg, stack, params, context: null, severity: 'error' });
+    } else {
+      const { message: msg, params } = this.extractMessages([message, context]);
+      this.printMessage({ message: msg, params, context: null, severity: 'error' });
+    }
+  }
+
+  /**
+   * Logs a fatal error message.
+   * @param message - The fatal error message or Error object.
+   * @param context - The context of the log message.
+   */
+  fatal(message: string | Error, context?: LogContext): void {
+    if (!this.isLevelEnabled('fatal')) return;
+
+    if (message instanceof Error) {
+      const { message: msg, stack, params } = this.extractMessagesWithStack([message, context]);
+      this.printMessage({ message: msg, stack, params, context: null, severity: 'fatal' });
+    } else {
+      const { message: msg, params } = this.extractMessages([message, context]);
+      this.printMessage({ message: msg, params, context: null, severity: 'fatal' });
+    }
+  }
+}
